0906051a23e84a78eae9faed17312c75b6e42b91
Charles Ewel <Charles.Ewel@gmail.com>
2016-08-30 14:54:12 -0700

    did work towards dynamic updating of boardstate

--- app/components/tritryst-game.js
+++ app/components/tritryst-game.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
 
+const Tile = Ember.Object.extend({
+
+})
+
 export default Ember.Component.extend({
+  horizontal: false,
+  firstColor: "empty",
+  secondColor: "empty",
+  thirdColor: "empty",
+
+  boardstate: [Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"}), Tile.create({state: "empty"})],
+
+  actions: {
+    selectTile(params) {
+      this.set("horizontal", params.horizontal);
+      this.set("firstColor", params.firstColor);
+      this.set("secondColor", params.secondColor);
+      this.set("thirdColor", params.thirdColor);
+    },
+    placeTile(location) {
+      if(this.get("secondColor")==="empty") {
+        alert("You forgot to set your tile");
+      } else {
+        this.get("boardstate").splice(location, 1);
+        this.get("boardstate").splice(location, 0, Tile.create({state: this.get("secondColor")}));
+      }
+    }
+  }
 });
