fbf611ecf9c955ae2c5e81684199e0efd2904d5f
Charles Ewel <Charles.Ewel@gmail.com>
2016-08-30 16:45:19 -0700

    allow tritryst to score around middle of the board correctly, special cases must be written for edge

--- app/components/tritryst-game.js
+++ app/components/tritryst-game.js
@@ -26,9 +26,47 @@ export default Ember.Component.extend({
       } else if (this.get("horizontal")) {
         if(location%8!==0 && location%8 !== 7) {
           if(this.get("boardstate")[location-1].state==="empty" && this.get("boardstate")[location+1].state==="empty") {
+            //Valid horizontal row placement
             this.get("boardstate").splice(location-1, 1, Tile.create({state: this.get("firstColor")}));
             this.get("boardstate").splice(location, 1, Tile.create({state: this.get("secondColor")}));
             this.get("boardstate").splice(location+1, 1, Tile.create({state: this.get("thirdColor")}));
+            //checks if center completed a vertical
+            var checkSquare=location;
+            if(this.get("boardstate")[checkSquare-8].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+8].state===this.get("boardstate")[checkSquare].state) {
+              this.set("score", this.get("score")+3)
+              this.get("boardstate").splice(location-8, 1, Tile.create({state: this.get("firstColor")}));
+              this.get("boardstate").splice(location, 1, Tile.create({state: this.get("secondColor")}));
+              this.get("boardstate").splice(location+8, 1, Tile.create({state: this.get("thirdColor")}));
+            }
+            //checks if lefthand side completed vertical then horizontal
+            checkSquare=location-1;
+            if(this.get("boardstate")[checkSquare-8].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+8].state===this.get("boardstate")[checkSquare].state) {
+              this.set("score", this.get("score")+3)
+              this.get("boardstate").splice(checkSquare-8, 1, Tile.create({state: this.get("firstColor")}));
+              this.get("boardstate").splice(checkSquare, 1, Tile.create({state: this.get("secondColor")}));
+              this.get("boardstate").splice(checkSquare+8, 1, Tile.create({state: this.get("thirdColor")}));
+            }
+            if(this.get("boardstate")[checkSquare-1].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+1].state===this.get("boardstate")[checkSquare].state) {
+              this.set("score", this.get("score")+3)
+              this.get("boardstate").splice(checkSquare-1, 1, Tile.create({state: this.get("firstColor")}));
+              this.get("boardstate").splice(checkSquare, 1, Tile.create({state: this.get("secondColor")}));
+              this.get("boardstate").splice(checkSquare+1, 1, Tile.create({state: this.get("thirdColor")}));
+            }
+            //checks if righthand side completed vertical then horizontal
+            checkSquare=location+1;
+            if(this.get("boardstate")[checkSquare-8].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+8].state===this.get("boardstate")[checkSquare].state) {
+              this.set("score", this.get("score")+3)
+              this.get("boardstate").splice(checkSquare-8, 1, Tile.create({state: this.get("firstColor")}));
+              this.get("boardstate").splice(checkSquare, 1, Tile.create({state: this.get("secondColor")}));
+              this.get("boardstate").splice(checkSquare+8, 1, Tile.create({state: this.get("thirdColor")}));
+            }
+            if(this.get("boardstate")[checkSquare-1].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+1].state===this.get("boardstate")[checkSquare].state) {
+              this.set("score", this.get("score")+3)
+              this.get("boardstate").splice(checkSquare-1, 1, Tile.create({state: this.get("firstColor")}));
+              this.get("boardstate").splice(checkSquare, 1, Tile.create({state: this.get("secondColor")}));
+              this.get("boardstate").splice(checkSquare+1, 1, Tile.create({state: this.get("thirdColor")}));
+            }
+            this.sendAction('processMove');
           } else {
             alert("Not a valid space (overlaps an existing color)");
           }
@@ -38,19 +76,54 @@ export default Ember.Component.extend({
       } else {
         if(location>7 && location<56) {
           if(this.get("boardstate")[location-8].state==="empty" && this.get("boardstate")[location+8].state==="empty") {
+            //valid vertical row placement
             this.get("boardstate").splice(location-8, 1, Tile.create({state: this.get("firstColor")}));
             this.get("boardstate").splice(location, 1, Tile.create({state: this.get("secondColor")}));
             this.get("boardstate").splice(location+8, 1, Tile.create({state: this.get("thirdColor")}));
+            //checks if center completed a horizontal
+            var checkSquare=location;
+            if(this.get("boardstate")[checkSquare-1].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+1].state===this.get("boardstate")[checkSquare].state) {
+              this.set("score", this.get("score")+3)
+              this.get("boardstate").splice(location-1, 1, Tile.create({state: this.get("firstColor")}));
+              this.get("boardstate").splice(location, 1, Tile.create({state: this.get("secondColor")}));
+              this.get("boardstate").splice(location+1, 1, Tile.create({state: this.get("thirdColor")}));
+            }
+            //checks if upper side completed vertical then horizontal
+            checkSquare=location-8;
+            if(this.get("boardstate")[checkSquare-8].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+8].state===this.get("boardstate")[checkSquare].state) {
+              this.set("score", this.get("score")+3)
+              this.get("boardstate").splice(checkSquare-8, 1, Tile.create({state: this.get("firstColor")}));
+              this.get("boardstate").splice(checkSquare, 1, Tile.create({state: this.get("secondColor")}));
+              this.get("boardstate").splice(checkSquare+8, 1, Tile.create({state: this.get("thirdColor")}));
+            }
+            if(this.get("boardstate")[checkSquare-1].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+1].state===this.get("boardstate")[checkSquare].state) {
+              this.set("score", this.get("score")+3)
+              this.get("boardstate").splice(checkSquare-1, 1, Tile.create({state: this.get("firstColor")}));
+              this.get("boardstate").splice(checkSquare, 1, Tile.create({state: this.get("secondColor")}));
+              this.get("boardstate").splice(checkSquare+1, 1, Tile.create({state: this.get("thirdColor")}));
+            }
+            //checks if lower side completed vertical then horizontal
+            checkSquare=location+8;
+            if(this.get("boardstate")[checkSquare-8].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+8].state===this.get("boardstate")[checkSquare].state) {
+              this.set("score", this.get("score")+3)
+              this.get("boardstate").splice(checkSquare-8, 1, Tile.create({state: this.get("firstColor")}));
+              this.get("boardstate").splice(checkSquare, 1, Tile.create({state: this.get("secondColor")}));
+              this.get("boardstate").splice(checkSquare+8, 1, Tile.create({state: this.get("thirdColor")}));
+            }
+            if(this.get("boardstate")[checkSquare-1].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+1].state===this.get("boardstate")[checkSquare].state) {
+              this.set("score", this.get("score")+3)
+              this.get("boardstate").splice(checkSquare-1, 1, Tile.create({state: this.get("firstColor")}));
+              this.get("boardstate").splice(checkSquare, 1, Tile.create({state: this.get("secondColor")}));
+              this.get("boardstate").splice(checkSquare+1, 1, Tile.create({state: this.get("thirdColor")}));
+            }
+            this.sendAction('processMove');
           } else {
             alert("Not a valid space (overlaps an existing color)");
           }
         } else {
           alert("Not a valid space (on the edge)");
         }
-        var checkSquare=location;
-        if(this.get("boardstate")[checkSquare-1].state===this.get("boardstate")[checkSquare].state && this.get("boardstate")[checkSquare+1].state===this.get("boardstate")[checkSquare].state) {
-          this.set("score", this.get("score")+3)
-        }
+
       }
 
     }
