57bcfaac0dad72b1c2f1758abe8ea71bfa840443
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-08-31 14:21:21 -0700

    sync audio

--- app/components/simon-game.js
+++ app/components/simon-game.js
@@ -15,10 +15,10 @@ export default Ember.Component.extend({
   actions: {
 
     newGame() {
-      var lablue = new Audio("sounds/2.mp3");
-      var lared = new Audio("sounds/4.mp3");
-      var lagreen = new Audio("sounds/3.mp3");
-      var layellow = new Audio("sounds/1.mp3");
+      var bluemp3 = new Audio("sounds/blue.mp3");
+      var redmp3 = new Audio("sounds/red.mp3");
+      var greenmp3 = new Audio("sounds/green.mp3");
+      var yellowmp3 = new Audio("sounds/yellow.mp3");
       var firstColor = Math.floor(Math.random() * 4);
       this.set("correctSequence", [this.get("colors")[firstColor]]);
       this.get("correctSequence").push("");
@@ -32,13 +32,13 @@ export default Ember.Component.extend({
         that.set('redOn', false);
         that.set(that.get("correctSequence")[iterator]+'On', true);
         if(that.get('yellowOn')) {
-          layellow.play();
+          yellowmp3.play();
         } else if (that.get('blueOn')) {
-          lablue.play();
+          bluemp3.play();
         } else if (that.get('greenOn')) {
-          lagreen.play();
+          greenmp3.play();
         } else if (that.get('redOn')) {
-          lared.play();
+          redmp3.play();
         }
         iterator++;
         if(iterator === that.get("correctSequence").length) {
@@ -50,18 +50,23 @@ export default Ember.Component.extend({
 
 
     guessColor(color) {
-      var lablue = new Audio("sounds/2.mp3");
-      var lared = new Audio("sounds/4.mp3");
-      var lagreen = new Audio("sounds/3.mp3");
-      var layellow = new Audio("sounds/1.mp3");
-      if (color==="red") {
-        lared.play();
-      } else if (color==="yellow") {
-        layellow.play();
-      } else if (color==="green") {
-        lagreen.play();
-      } else if (color==="blue") {
-        lablue.play();
+      var bluemp3 = new Audio("sounds/blue.mp3");
+      var redmp3 = new Audio("sounds/red.mp3");
+      var greenmp3 = new Audio("sounds/green.mp3");
+      var yellowmp3 = new Audio("sounds/yellow.mp3");
+      var that = this;
+      that.set((color+'On'),true);
+      Ember.run.later(function(){
+        that.set((color+'On'),false);
+        }, 1000);
+      if (color === "red") {
+        redmp3.play();
+      } else if (color === "blue") {
+        bluemp3.play();
+      } else if (color === "yellow") {
+        yellowmp3.play();
+      } else if (color === "green") {
+        greenmp3.play();
       }
       if(this.get("buttonsLive")) {
         var guessNum = this.get('guessNumber');
@@ -94,7 +99,6 @@ export default Ember.Component.extend({
             this.get("correctSequence").push(this.get("colors")[nextColor]);
             this.get("correctSequence").push("");
             var iterator=0;
-            var that = this;
             this.set("buttonsLive", false);
             var displaySequence = setInterval(function() {
               that.set('blueOn', false);
@@ -103,13 +107,13 @@ export default Ember.Component.extend({
               that.set('redOn', false);
               that.set(that.get("correctSequence")[iterator]+'On', true);
               if(that.get('yellowOn')) {
-                layellow.play();
+                yellowmp3.play();
               } else if (that.get('blueOn')) {
-                lablue.play();
+                bluemp3.play();
               } else if (that.get('greenOn')) {
-                lagreen.play();
+                greenmp3.play();
               } else if (that.get('redOn')) {
-                lared.play();
+                redmp3.play();
               }
               iterator++;
               if(iterator === that.get("correctSequence").length) {
